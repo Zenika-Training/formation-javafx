1182def379033bc23be33947b37203ec9759348b
Benjamin Houdu <benjamin.houdu@gmail.com>
2014-03-06 11:02:20 +0100

    Add ressources.

---
+++ Formation/TP_RESSOURCES/Zwitter/src/com/zenika/fx/zwitter/MainController.java
@@ -0,0 +1,42 @@
+package com.zenika.fx.zwitter;
+
+import java.util.LinkedList;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+import com.zenika.fx.zwitter.client.ZwitterClient;
+import com.zenika.fx.zwitter.model.Zweet;
+
+public class MainController {
+
+  private ZwitterClient client;
+
+  public void initialize() {
+    // ceci est un exemple d'utilisation du ZwitterSDK
+    ObservableList<Zweet> timeline = FXCollections.observableList(new LinkedList<Zweet>());
+    timeline.addListener(new ListChangeListener<Zweet>() {
+      @Override
+      public void onChanged(javafx.collections.ListChangeListener.Change<? extends Zweet> c) {
+        while (c.next()) {
+          if (c.wasAdded()) {
+            for (Zweet zweet : c.getAddedSubList()) {
+              System.out.println(zweet);
+            }
+          }
+        }
+      }
+    });
+
+    client = new ZwitterClient();
+    client.startTimeline(timeline);
+  }
+
+  public void stop() {
+    client.terminate();
+  }
+
+}
