85130f928903e983f08d8e81395bf29fef5c5ae2
Benjamin Houdu <benjamin.houdu@gmail.com>
2014-03-12 22:42:11 +0100

    Add TP7.

---
+++ Formation/ZwitterTP/TP7/src/com/zenika/fx/zwitter/SearchTask.java
@@ -0,0 +1,32 @@
+package com.zenika.fx.zwitter;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.zenika.fx.zwitter.model.Zweet;
+import javafx.concurrent.Task;
+
+public class SearchTask extends Task<Set<Zweet>> {
+
+    private final List<Zweet> source;
+    private final String keyword;
+
+    public SearchTask(final List<Zweet> source, final String keyword) {
+        this.source = source;
+        this.keyword = keyword;
+    }
+
+    @Override
+    protected Set<Zweet> call() throws Exception {
+        final Set<Zweet> result = new HashSet<>(source.size());
+        for (final Zweet zweet : source) {
+            Thread.sleep(100);
+            if (zweet.getText().contains(keyword)) {
+                result.add(zweet);
+            }
+        }
+        return result;
+    }
+
+}
