a6fc9f1bd1aa9310dee79a700957b29eabdf00a6
Benjamin Houdu <benjamin.houdu@gmail.com>
2014-11-03 17:36:19 +0100

    Update TP4.

--- Formation/ZwitterTP/TP4/src/com/zenika/fx/zwitter/MainController.java
+++ Formation/ZwitterTP/TP4/src/com/zenika/fx/zwitter/MainController.java
@@ -20,53 +20,53 @@ import javafx.util.Duration;
 
 public class MainController implements Initializable {
 
-    @FXML
-    private ListView<Zweet> timeline;
+	@FXML
+	private ListView<Zweet> timeline;
 
-    private Zwitter zwitter;
+	private Zwitter zwitter;
 
-    @Override
-    public void initialize(final URL url, final ResourceBundle resourceBundle) {
-        // ceci est un exemple d'utilisation du ZwitterSDK
-        final ObservableList<Zweet> list = FXCollections.observableList(new LinkedList<Zweet>());
-        timeline.setItems(list);
+	@Override
+	public void initialize(final URL url, final ResourceBundle resourceBundle) {
+		// ceci est un exemple d'utilisation du ZwitterSDK
+		final ObservableList<Zweet> list = FXCollections.observableList(new LinkedList<>());
+		timeline.setItems(list);
 
-        timeline.setCellFactory(new ZweetCell());
+		timeline.setCellFactory(new ZweetCell());
 
-        zwitter = ZwitterBuilder.create().withObservableList(list).build();
-        zwitter.start();
-    }
+		zwitter = ZwitterBuilder.create().withObservableList(list).build();
+		zwitter.start();
+	}
 
-    @FXML
-    public void start() {
-        zwitter.start();
-    }
+	@FXML
+	public void start() {
+		zwitter.start();
+	}
 
-    @FXML
-    public void stop() {
-        zwitter.stop();
-    }
+	@FXML
+	public void stop() {
+		zwitter.stop();
+	}
 
-    public static Transition createTransition(final Node pane) {
-        final FadeTransition fade = new FadeTransition(Duration.seconds(1d), pane);
-        fade.setFromValue(0.5d);
-        fade.setToValue(1d);
+	public static Transition createTransition(final Node pane) {
+		final FadeTransition fade = new FadeTransition(Duration.millis(100), pane);
+		fade.setFromValue(0.5d);
+		fade.setToValue(1d);
 
-        final ScaleTransition scale1 = new ScaleTransition(Duration.millis(500d), pane);
-        scale1.setFromX(0d);
-        scale1.setToX(0.5);
-        scale1.setFromY(0d);
-        scale1.setToY(0.5);
+		final ScaleTransition scale1 = new ScaleTransition(Duration.millis(50d), pane);
+		scale1.setFromX(0d);
+		scale1.setToX(0.5);
+		scale1.setFromY(0d);
+		scale1.setToY(0.5);
 
-        final ScaleTransition scale2 = new ScaleTransition(Duration.millis(500d), pane);
-        scale2.setFromX(0.5);
-        scale2.setFromY(0.5);
-        scale2.setToX(1d);
-        scale2.setToY(1d);
-        scale2.setCycleCount(3);
-        scale2.setAutoReverse(true);
+		final ScaleTransition scale2 = new ScaleTransition(Duration.millis(50d), pane);
+		scale2.setFromX(0.5);
+		scale2.setFromY(0.5);
+		scale2.setToX(1d);
+		scale2.setToY(1d);
+		scale2.setCycleCount(3);
+		scale2.setAutoReverse(true);
 
-        return new ParallelTransition(fade, new SequentialTransition(scale1, scale2));
-    }
+		return new ParallelTransition(fade, new SequentialTransition(scale1, scale2));
+	}
 
 }
