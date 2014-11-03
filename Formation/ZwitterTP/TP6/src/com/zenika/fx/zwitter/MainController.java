4a8f21f4df7020bd5a10fff316c614bd072ba52c
Benjamin Houdu <benjamin.houdu@gmail.com>
2014-11-03 17:43:05 +0100

    Update TP6.

--- Formation/ZwitterTP/TP6/src/com/zenika/fx/zwitter/MainController.java
+++ Formation/ZwitterTP/TP6/src/com/zenika/fx/zwitter/MainController.java
@@ -27,100 +27,97 @@ import javafx.util.Duration;
 
 public class MainController implements Initializable {
 
-    @FXML
-    private ListView<Zweet> timeline;
+	@FXML
+	private ListView<Zweet> timeline;
 
-    @FXML
-    private TextArea zweetArea;
+	@FXML
+	private TextArea zweetArea;
 
-    @FXML
-    private Label zweetCompletionText;
+	@FXML
+	private Label zweetCompletionText;
 
-    @FXML
-    private ProgressBar zweetCompletion;
+	@FXML
+	private ProgressBar zweetCompletion;
 
-    private Zwitter zwitter;
+	private Zwitter zwitter;
 
-    @Override
-    public void initialize(final URL url, final ResourceBundle resourceBundle) {
-        final ObservableList<Zweet> list = FXCollections.observableList(new LinkedList<Zweet>());
-        timeline.setItems(list);
+	@Override
+	public void initialize(final URL url, final ResourceBundle resourceBundle) {
+		final ObservableList<Zweet> list = FXCollections.observableList(new LinkedList<>());
+		timeline.setItems(list);
 
-        timeline.setCellFactory(new ZweetCell());
+		timeline.setCellFactory(new ZweetCell());
 
-        zwitter = ZwitterBuilder.create().withObservableList(list).build();
-        zwitter.start();
+		zwitter = ZwitterBuilder.create().withObservableList(list).build();
+		zwitter.start();
 
-        final IntegerBinding zweetLengthProperty = new IntegerBinding() {
-            {
-                bind(zweetArea.textProperty());
-            }
+		final IntegerBinding zweetLengthProperty = new IntegerBinding() {
+			{
+				bind(zweetArea.textProperty());
+			}
 
-            @Override
-            protected int computeValue() {
-                if (null == zweetArea.getText() || zweetArea.getText().isEmpty()) {
-                    return 0;
-                }
-                return zweetArea.getText().length();
-            }
-        };
+			@Override
+			protected int computeValue() {
+				if (null == zweetArea.getText() || zweetArea.getText().isEmpty()) {
+					return 0;
+				}
+				return zweetArea.getText().length();
+			}
+		};
 
-        zweetCompletionText.textProperty().bind(zweetLengthProperty.asString());
-        zweetCompletion.progressProperty().bind(zweetLengthProperty.divide(140d));
+		zweetCompletionText.textProperty().bind(zweetLengthProperty.asString());
+		zweetCompletion.progressProperty().bind(zweetLengthProperty.divide(140d));
 
-        zweetCompletion.getStyleClass().add("completion");
-        zweetCompletionText.getStyleClass().add("completion");
-        zweetLengthProperty.addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(final ObservableValue<? extends Number> paramObservableValue, final Number oldValue, final Number newValue) {
-                if (140 < newValue.intValue()) {
-                    zweetCompletion.getStyleClass().addAll("overflow");
-                    zweetCompletionText.getStyleClass().addAll("overflow");
-                } else {
-                    zweetCompletion.getStyleClass().removeAll("overflow");
-                    zweetCompletionText.getStyleClass().removeAll("overflow");
-                }
-            }
-        });
-    }
+		zweetCompletion.getStyleClass().add("completion");
+		zweetCompletionText.getStyleClass().add("completion");
+		zweetLengthProperty.addListener((paramObservableValue, oldValue, newValue) -> {
+			if (140 < newValue.intValue()) {
+				zweetCompletion.getStyleClass().addAll("overflow");
+				zweetCompletionText.getStyleClass().addAll("overflow");
+			} else {
+				zweetCompletion.getStyleClass().removeAll("overflow");
+				zweetCompletionText.getStyleClass().removeAll("overflow");
+			}
+		});
+	}
 
-    @FXML
-    public void start() {
-        zwitter.start();
-    }
+	@FXML
+	public void start() {
+		zwitter.start();
+	}
 
-    @FXML
-    public void stop() {
-        zwitter.stop();
-    }
+	@FXML
+	public void stop() {
+		zwitter.stop();
+	}
 
-    @FXML
-    public void publish() {
-        final ZwitterUser zwitterUser = new ZwitterUser("", "", "");
-        final Zweet zweet = new Zweet(zwitterUser, zweetArea.getText());
-        zwitter.publish(zweet);
-    }
+	@FXML
+	public void publish() {
+		final ZwitterUser zwitterUser = new ZwitterUser("", "", "");
+		final Zweet zweet = new Zweet(zwitterUser, zweetArea.getText());
+		zwitter.publish(zweet);
+	}
 
-    public static Transition createTransition(final Node pane) {
-        final FadeTransition fade = new FadeTransition(Duration.seconds(1d), pane);
-        fade.setFromValue(0.5d);
-        fade.setToValue(1d);
+	public static Transition createTransition(final Node pane) {
+		final FadeTransition fade = new FadeTransition(Duration.seconds(1d), pane);
+		fade.setFromValue(0.5d);
+		fade.setToValue(1d);
 
-        final ScaleTransition scale1 = new ScaleTransition(Duration.millis(500d), pane);
-        scale1.setFromX(0d);
-        scale1.setToX(0.5);
-        scale1.setFromY(0d);
-        scale1.setToY(0.5);
+		final ScaleTransition scale1 = new ScaleTransition(Duration.millis(500d), pane);
+		scale1.setFromX(0d);
+		scale1.setToX(0.5);
+		scale1.setFromY(0d);
+		scale1.setToY(0.5);
 
-        final ScaleTransition scale2 = new ScaleTransition(Duration.millis(500d), pane);
-        scale2.setFromX(0.5);
-        scale2.setFromY(0.5);
-        scale2.setToX(1d);
-        scale2.setToY(1d);
-        scale2.setCycleCount(3);
-        scale2.setAutoReverse(true);
+		final ScaleTransition scale2 = new ScaleTransition(Duration.millis(500d), pane);
+		scale2.setFromX(0.5);
+		scale2.setFromY(0.5);
+		scale2.setToX(1d);
+		scale2.setToY(1d);
+		scale2.setCycleCount(3);
+		scale2.setAutoReverse(true);
 
-        return new ParallelTransition(fade, new SequentialTransition(scale1, scale2));
-    }
+		return new ParallelTransition(fade, new SequentialTransition(scale1, scale2));
+	}
 
 }
--- Formation/ZwitterTP/TP6/src/com/zenika/fx/zwitter/SearchBox.java
+++ Formation/ZwitterTP/TP6/src/com/zenika/fx/zwitter/SearchBox.java
@@ -1,56 +1,43 @@
 package com.zenika.fx.zwitter;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
 
 public class SearchBox extends Region {
 
-    private final TextField searchText;
-    private final Button clearButton;
+	private final TextField searchText;
+	private final Button clearButton;
 
-    public SearchBox() {
-        setId("SearchBox");
-        setMinHeight(24);
-        setPrefSize(150, 24);
-        setMaxHeight(24);
+	public SearchBox() {
+		setId("SearchBox");
+		setMinHeight(24);
+		setPrefSize(150, 24);
+		setMaxHeight(24);
 
-        searchText = new TextField();
-        searchText.setPromptText("Rechercher");
-        searchText.prefWidthProperty().bind(widthProperty().subtract(20d));
-        searchText.getStyleClass().add("search");
+		searchText = new TextField();
+		searchText.setPromptText("Rechercher");
+		searchText.prefWidthProperty().bind(widthProperty().subtract(20d));
+		searchText.getStyleClass().add("search");
 
-        clearButton = new Button();
-        clearButton.setVisible(false);
-        clearButton.getStyleClass().add("clearSearch");
+		clearButton = new Button();
+		clearButton.setVisible(false);
+		clearButton.getStyleClass().add("clearSearch");
 
-        getChildren().addAll(searchText, clearButton);
+		getChildren().addAll(searchText, clearButton);
 
-        clearButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(final ActionEvent paramT) {
-                searchText.clear();
-                searchText.requestFocus();
-            }
-        });
+		clearButton.setOnAction(paramT -> {
+			searchText.clear();
+			searchText.requestFocus();
+		});
 
-        searchText.textProperty().addListener(new ChangeListener<String>() {
+		searchText.textProperty().addListener((paramObservableValue, oldValue, newValue) -> clearButton.setVisible(null != newValue && !newValue.isEmpty()));
+	}
 
-            @Override
-            public void changed(final ObservableValue<? extends String> paramObservableValue, final String oldValue, final String newValue) {
-                clearButton.setVisible(null != newValue && !newValue.isEmpty());
-            }
-        });
-    }
-
-    @Override
-    protected void layoutChildren() {
-        searchText.resize(getWidth(), getHeight());
-        clearButton.resizeRelocate(getWidth() - 18, 6, 12, 13);
-    }
+	@Override
+	protected void layoutChildren() {
+		searchText.resize(getWidth(), getHeight());
+		clearButton.resizeRelocate(getWidth() - 18, 6, 12, 13);
+	}
 
 }
