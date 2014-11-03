9f18ca9516f051b9f77b6f318502721727070761
Benjamin Houdu <benjamin.houdu@gmail.com>
2014-11-03 15:18:41 +0100

    Format code.

--- Formation/ZwitterAPI/src/com/zenika/fx/zwitter/ZwitterBuilder.java
+++ Formation/ZwitterAPI/src/com/zenika/fx/zwitter/ZwitterBuilder.java
@@ -11,64 +11,64 @@ import javafx.event.EventHandler;
 
 public class ZwitterBuilder {
 
-    private Set<EventHandler<WorkerStateEvent>> onSuccessHandlers = new HashSet<>();
-    private double delay = 1000;
-    private double period = 2000;
+	private Set<EventHandler<WorkerStateEvent>> onSuccessHandlers = new HashSet<>();
+	private double delay = 1000;
+	private double period = 2000;
 
-    private ZwitterBuilder() {
-        final EventHandler<WorkerStateEvent> onSuccessHandler = new EventHandler<WorkerStateEvent>() {
-            @Override
-            public void handle(final WorkerStateEvent workerStateEvent) {
-                final Object value = workerStateEvent.getSource().getValue();
-                System.out.println("Got new values for Zwitter:" + value);
-            }
-        };
-        onSuccessHandlers.add(onSuccessHandler);
-    }
+	private ZwitterBuilder() {
+		final EventHandler<WorkerStateEvent> onSuccessHandler = new EventHandler<WorkerStateEvent>() {
+			@Override
+			public void handle(final WorkerStateEvent workerStateEvent) {
+				final Object value = workerStateEvent.getSource().getValue();
+				System.out.println("Got new values for Zwitter:" + value);
+			}
+		};
+		onSuccessHandlers.add(onSuccessHandler);
+	}
 
-    public static ZwitterBuilder create() {
-        return new ZwitterBuilder();
-    }
+	public static ZwitterBuilder create() {
+		return new ZwitterBuilder();
+	}
 
-    public ZwitterBuilder withOnSuccessHandler(final EventHandler<WorkerStateEvent> onSuccessHandler) {
-        onSuccessHandlers.add(onSuccessHandler);
-        return this;
-    }
+	public ZwitterBuilder withOnSuccessHandler(final EventHandler<WorkerStateEvent> onSuccessHandler) {
+		onSuccessHandlers.add(onSuccessHandler);
+		return this;
+	}
 
-    public ZwitterBuilder withObservableList(final ObservableList<Zweet> zweets) {
-        return withObservableList(zweets, -1);
-    }
+	public ZwitterBuilder withObservableList(final ObservableList<Zweet> zweets) {
+		return withObservableList(zweets, -1);
+	}
 
-    public ZwitterBuilder withObservableList(final ObservableList<Zweet> zweets, final int maxElements) {
-        final EventHandler<WorkerStateEvent> onSuccessHandler = new EventHandler<WorkerStateEvent>() {
-            @Override
-            public void handle(final WorkerStateEvent workerStateEvent) {
-                final List<Zweet> zweetsFromWorker = (List<Zweet>) workerStateEvent.getSource().getValue();
-                for (int i = zweetsFromWorker.size() - 1; i >= 0; i--) {
-                    final Zweet zweet = zweetsFromWorker.get(i);
-                    zweets.add(0, zweet);
-                }
-                if (maxElements > 0 && zweets.size() > maxElements) {
-                    zweets.remove(maxElements, zweets.size());
-                }
-            }
-        };
-        onSuccessHandlers.add(onSuccessHandler);
-        return this;
-    }
+	public ZwitterBuilder withObservableList(final ObservableList<Zweet> zweets, final int maxElements) {
+		final EventHandler<WorkerStateEvent> onSuccessHandler = new EventHandler<WorkerStateEvent>() {
+			@Override
+			public void handle(final WorkerStateEvent workerStateEvent) {
+				final List<Zweet> zweetsFromWorker = (List<Zweet>) workerStateEvent.getSource().getValue();
+				for (int i = zweetsFromWorker.size() - 1; i >= 0; i--) {
+					final Zweet zweet = zweetsFromWorker.get(i);
+					zweets.add(0, zweet);
+				}
+				if (maxElements > 0 && zweets.size() > maxElements) {
+					zweets.remove(maxElements, zweets.size());
+				}
+			}
+		};
+		onSuccessHandlers.add(onSuccessHandler);
+		return this;
+	}
 
-    public ZwitterBuilder withDelay(final double delay) {
-        this.delay = delay;
-        return this;
-    }
+	public ZwitterBuilder withDelay(final double delay) {
+		this.delay = delay;
+		return this;
+	}
 
-    public ZwitterBuilder withPeriod(final double period) {
-        this.period = period;
-        return this;
-    }
+	public ZwitterBuilder withPeriod(final double period) {
+		this.period = period;
+		return this;
+	}
 
-    public Zwitter build() {
-        return new ZwitterImpl(delay, period, onSuccessHandlers);
-    }
+	public Zwitter build() {
+		return new ZwitterImpl(delay, period, onSuccessHandlers);
+	}
 
 }
