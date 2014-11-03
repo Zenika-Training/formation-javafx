6e56dd59b99b6b78fa52b97eaf1ed8c0036361a2
Benjamin Houdu <benjamin.houdu@gmail.com>
2014-11-03 17:39:43 +0100

    Update TP5.

--- Formation/ZwitterTP/TP5/src/com/zenika/fx/zwitter/ZweetCell.java
+++ Formation/ZwitterTP/TP5/src/com/zenika/fx/zwitter/ZweetCell.java
@@ -8,35 +8,37 @@ import javafx.util.Callback;
 
 public class ZweetCell implements Callback<ListView<Zweet>, ListCell<Zweet>> {
 
-    @Override
-    public ListCell<Zweet> call(final ListView<Zweet> zweetListView) {
-        final ListCell<Zweet> cell = new ListCell<Zweet>() {
+	@Override
+	public ListCell<Zweet> call(final ListView<Zweet> zweetListView) {
+		final ListCell<Zweet> cell = new ListCell<Zweet>() {
 
-            private ZweetPane zweetPane;
+			private ZweetPane zweetPane;
 
-            @Override
-            public void updateItem(final Zweet item, final boolean empty) {
-                super.updateItem(item, empty);
-                if (!isEmpty()) {
-                    if (null == zweetPane) {
-                        zweetPane = new ZweetPane(item);
-                        zweetPane.prefWidthProperty().bind(zweetListView.widthProperty().subtract(74d));
-                    } else {
-                        zweetPane.setZweet(item);
-                    }
-                    setGraphic(zweetPane);
+			@Override
+			public void updateItem(final Zweet item, final boolean empty) {
+				super.updateItem(item, empty);
+				if (!isEmpty()) {
+					if (null == zweetPane) {
+						zweetPane = new ZweetPane(item);
+						zweetPane.prefWidthProperty().bind(zweetListView.widthProperty().subtract(74d));
+					} else {
+						zweetPane.setZweet(item);
+					}
+					setGraphic(zweetPane);
 
-                    if (!zweetListView.getItems().isEmpty()) {
-                        final Zweet zweet = zweetListView.getItems().get(0);
-                        if (zweet == item) {
-                            final Transition transition = MainController.createTransition(zweetPane);
-                            transition.playFromStart();
-                        }
-                    }
-                }
-            }
-        };
-        return cell;
-    }
+					if (!zweetListView.getItems().isEmpty()) {
+						final Zweet zweet = zweetListView.getItems().get(0);
+						if (zweet == item) {
+							final Transition transition = MainController.createTransition(zweetPane);
+							transition.playFromStart();
+						}
+					}
+				} else {
+					setGraphic(null);
+				}
+			}
+		};
+		return cell;
+	}
 
 }
