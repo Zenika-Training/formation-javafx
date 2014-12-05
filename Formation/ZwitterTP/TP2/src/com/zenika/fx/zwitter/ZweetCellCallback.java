4edf229e1bdc1dcc96a215c3777da9e3f37fb3bd
Benjamin Houdu <benjamin.houdu@gmail.com>
2014-12-05 10:12:36 +0100

    Optimize callback.

--- Formation/ZwitterTP/TP2/src/com/zenika/fx/zwitter/ZweetCellCallback.java
+++ Formation/ZwitterTP/TP2/src/com/zenika/fx/zwitter/ZweetCellCallback.java
@@ -11,15 +11,19 @@ public class ZweetCellCallback implements Callback<ListView<Zweet>, ListCell<Zwe
 	@Override
 	public ListCell<Zweet> call(final ListView<Zweet> zweetListView) {
 		final ListCell<Zweet> cell = new ListCell<Zweet>() {
+
 			private Text text;
 
 			@Override
 			public void updateItem(final Zweet item, final boolean empty) {
 				super.updateItem(item, empty);
 				if (!isEmpty()) {
-					text = new Text(item.toString());
-					text.wrappingWidthProperty().bind(zweetListView.widthProperty().subtract(300));
-					setGraphic(text);
+					if (text == null) {
+						text = new Text();
+						text.wrappingWidthProperty().bind(zweetListView.widthProperty().subtract(40));
+						setGraphic(text);
+					}
+					text.setText(item.toString());
 				}
 			}
 		};
